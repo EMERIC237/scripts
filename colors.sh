f087e16f437598cc4a494a9003325c9cf7d7bc6c
root <root@station.domain9.example.com>
2020-05-21 08:52:56 -0700

    color+file

---
+++ colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Example scripts
+
+# Run the constants file, like an import
+source ./config.sh
+
+echo "\n ${RED} Text in RED  \n"
+echo "\n ${GREEN} Text in GREEN  \n"
+echo "\n ${ORANGE} Text in ORANGE  \n"
+echo "\n ${BLUE} Text in BLUE  \n"
+echo "\n ${PURPLE} Text in PURPLE  \n"
+echo "\n ${CYAN} Text in CYAN  \n"
+echo "\n ${LIGHT_GRAY} Text in LIGHT_GRAY  \n"
+echo "\n ${DARK_GRAY} Text in DARK_GRAY  \n"
+echo "\n ${LIGHT_RED} Text in LIGHT_RED  \n"
+echo "\n ${WHITE} Text in WHITE  \n"
